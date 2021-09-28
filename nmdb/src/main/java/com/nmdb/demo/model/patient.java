65b1a98bc9b3d7093c21543de08dc81993c2e2e8
Umarwalid <89044342+Umarwalid@users.noreply.github.com>
2021-09-28 15:58:26 +0100

    Add files via upload

---
+++ nmdb/src/main/java/com/nmdb/demo/model/patient.java
@@ -0,0 +1,85 @@
+package com.nmdb.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SecondaryTable;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name="patient")
+//@SecondaryTable(name="records")
+public class patient {
+
+	
+	@Id
+    @Column(name = "medicalno")
+	private int id;
+	
+	private String firstname;
+	private String lastname;
+	private String othername;
+	private String gender;
+	private String dob;
+	private String nationality;
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getOthername() {
+		return othername;
+	}
+	public void setOthername(String othername) {
+		this.othername = othername;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	@Override
+	public String toString() {
+		return "patient [Medicalno=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", othername=" + othername
+				+ ", gender=" + gender + ", dob=" + dob + ", nationality=" + nationality + "]";
+	}
+
+}
+
