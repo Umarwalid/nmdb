86925f74d013658c6fd67596b3f43c064929cd02
Umarwalid <89044342+Umarwalid@users.noreply.github.com>
2021-09-28 15:56:27 +0100

    Delete nmdb directory

--- nmdb/target/classes/Data.sql
+++
@@ -1,7 +0,0 @@
-
-insert into patient(medicalno,nationality,dob,firstname,gender,lastname,othername) values(40,'Nigerian', '1st october 2000','sam','male','alex',null);
-insert into patient(medicalno,nationality,dob,firstname,gender,lastname,othername) values(50,'Nigerian','2nd november 2001','name','female','musa','mina');
-insert into patient(medicalno,nationality,dob,firstname,gender ,lastname,othername) values(60,'Nigerian','3rd june 1990','adam','male','muses','john');
-insert into records(medicalno,hospital,day,purpose,symptoms,dignosis,treatment,remark) values(40, 'National hosiptal','15thjune2020','scan','swollen arm','fracture bone','non','non');
-insert into records(medicalno,hospital,day,purpose,symptoms,dignosis,treatment,remark) values(50,'New hospital','3rdmay2019','dignosis','high fever','malaria','anti biotics','non');
-insert into records(medicalno,hospital,day,purpose,symptoms,dignosis,treatment,remark) values(60,'Medicare','1stapril2010','eye test','unclear vision','shortsightedness','-3.0 glasses','non');
